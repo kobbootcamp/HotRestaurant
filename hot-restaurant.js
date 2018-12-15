54f453c3ef94c62e2532e1c41c08ecd8f8d985a0
Kevin <kobbootcamp@gmail.com>
2018-12-15 11:20:55 -0600

    updated hot-restaurant.js

--- hot-restaurant.js
+++ hot-restaurant.js
@@ -1,53 +1,51 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 var PORT = 3000;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname,"home.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname,"tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname,"reserve.html"));
+});
+
+app.get("/api/tables", function(req,res) {
+    return(res.json(sitting));
+});
+
+app.get("/api/waitlist", function(req,res) {
+    return(res.json(waiting));
+});
+
+app.listen(PORT,function(){
+    console.log("Now listening on port " + PORT)
+});
 
 var sitting = [
     {
-        name: "",
-        phone: "",
-        email: "",
-        guestId, ""
-
-
-
+        name:"Stevey Boye",
+        phone:"1234567890",
+        email:"asjldkvhasdlkg@kljsh.com",
+        guestId:"123456"
     }
-]
+];
 
 var waiting = [
     {
-        name: "",
-        phone: "",
-        email: "",
-        guestId: ""
-
+        name:"Big Ben",
+        phone:"87235623489",
+        email:"iurlg@lairsj.com",
+        guestId:"3427856"
     }
-]
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
+];
 
-app.get("/tables", function (req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
-
-});
-app.get("/reserve", function (req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-
-});
-
-app.get("/api/tables", function (req, res) {
-    // res.sendFile(path.join(__dirname, "reserve.html"));
-
-});
-
-app.get("/api/waitlist", function (req, res) {
-    // res.sendFile(path.join(__dirname, "reserve.html"));
-
-});
-
-app.listen(PORT, function () {
-    console.log("Now listening on port " + PORT)
-});
