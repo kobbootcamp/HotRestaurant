e542688b8ca78559890b967e9bd69d13d4d6e9dd
Kevin <kobbootcamp@gmail.com>
2018-12-15 11:01:09 -0600

    initialization2

--- hot-restaurant.js
+++ hot-restaurant.js
@@ -4,9 +4,28 @@ var app = express();
 var PORT = 3000;
 
 app.get("/", function(req, res) {
-    res.send("Welcome to our hot restaurant!");
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+    
+});
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+
+});
+
+app.get("/api/tables", function(req, res) {
+    // res.sendFile(path.join(__dirname, "reserve.html"));
+
+});
+
+app.get("/api/waitlist", function(req, res) {
+    // res.sendFile(path.join(__dirname, "reserve.html"));
+
+});
+  
 app.listen(PORT,function(){
     console.log("Now listening on port " + PORT)
 });
