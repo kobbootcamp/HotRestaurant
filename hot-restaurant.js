f2c5ca05955915f8ae199922d789cae2f114c744
Kevin <kobbootcamp@gmail.com>
2018-12-15 11:06:14 -0600

    added tables

--- hot-restaurant.js
+++ hot-restaurant.js
@@ -3,29 +3,51 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
-app.get("/", function(req, res) {
+
+var sitting = [
+    {
+        name: "",
+        phone: "",
+        email: "",
+        guestId, ""
+
+
+
+    }
+]
+
+var waiting = [
+    {
+        name: "",
+        phone: "",
+        email: "",
+        guestId: ""
+
+    }
+]
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
-    
+
 });
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 
 });
 
-app.get("/api/tables", function(req, res) {
+app.get("/api/tables", function (req, res) {
     // res.sendFile(path.join(__dirname, "reserve.html"));
 
 });
 
-app.get("/api/waitlist", function(req, res) {
+app.get("/api/waitlist", function (req, res) {
     // res.sendFile(path.join(__dirname, "reserve.html"));
 
 });
-  
-app.listen(PORT,function(){
+
+app.listen(PORT, function () {
     console.log("Now listening on port " + PORT)
 });
